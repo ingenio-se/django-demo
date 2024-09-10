f40b253cbc0454d727270dad5328162138eefce6
David Guerra <davidguerra@Davids-iMac.local>
2024-09-10 18:37:04 -0500

    relacion de muchos a muchos

--- demo/first_app/models.py
+++ demo/first_app/models.py
@@ -14,10 +14,19 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
+class Author(models.Model):
+    first_name = models.TextField(max_length=100)
+    last_name = models.TextField(max_length=100)
+    email = models.EmailField(max_length=100)
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 class Book(models.Model):
     title = models.TextField(max_length=100)
     author = models.TextField(max_length=100)
     publication_date = models.DateField()
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author,related_name='authors')
     def __str__(self):
         return self.title
+    

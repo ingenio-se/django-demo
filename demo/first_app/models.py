d03846c555fdaa32ba8fa8ae77b8c9d7f33d6168
David Guerra <davidguerra@Davids-iMac.local>
2024-09-10 18:07:16 -0500

    insertar dastos con django

--- demo/first_app/models.py
+++ demo/first_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     title = models.TextField(max_length=100)
+    year = models.TextField(max_length=4,null=True)
     
     def __str__(self):
-        pass
+        return self.title + ' - ' + self.year
